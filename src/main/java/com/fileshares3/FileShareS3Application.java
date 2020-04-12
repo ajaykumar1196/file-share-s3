73ae60d32338b4e8356c7d06662d0a0beba35eec
ajaykumar <ajaykumar1196@gmail.com>
2020-04-12 16:14:21 +0530

    initial commit

--- src/main/java/com/fileshares3/FileShareS3Application.java
+++ src/main/java/com/fileshares3/FileShareS3Application.java
@@ -1,4 +1,4 @@
-package com.fileshares3.fileshares3;
+package com.fileshares3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
