eafe8ef70d31c4e62a418be2af3ad2b4d3081eb8
ajaykumar <ajaykumar1196@gmail.com>
2020-04-12 16:10:05 +0530

    initial commit

---
+++ src/main/java/com/fileshares3/fileshares3/FileShareS3Application.java
@@ -0,0 +1,13 @@
+package com.fileshares3.fileshares3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileShareS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileShareS3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fileshares3/fileshares3/FileShareS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileshares3.fileshares3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileShareS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
